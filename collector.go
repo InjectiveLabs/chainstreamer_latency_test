c2dd75c4160242228097db684877d697630d2cd5
Michele Meloni <cleaversdev@gmail.com>
2024-02-12 23:03:28 +0100

    add last received

--- collector.go
+++ collector.go
@@ -37,6 +37,7 @@ func NewBlocksCollector(influxWriteAPI api.WriteAPI) *blocksCollector {
 }
 
 func (b *blocksCollector) PushBlock(ctx context.Context, blockChannel chan *Block, source string, influxWriteAPI api.WriteAPI) error {
+	var lastReceived time.Time
 	for {
 		select {
 		case <-ctx.Done():
@@ -45,9 +46,15 @@ func (b *blocksCollector) PushBlock(ctx context.Context, blockChannel chan *Bloc
 			if influxWriteAPI != nil {
 				lat := event.ReceivedAt.Sub(event.BlockTimestamp).Milliseconds()
 				p := influxdb2.NewPointWithMeasurement("streamer_lat")
+				p = p.AddTag("source", source)
 				p = p.AddField(fmt.Sprintf("%s_lat_ms", source), lat)
 				p = p.AddField("height", event.BlockHeight)
-				p = p.SetTime(time.Now())
+				p = p.AddField("received_at", event.ReceivedAt)
+				if !lastReceived.IsZero() {
+					p = p.AddField("ms_from_last_received", event.ReceivedAt.Sub(lastReceived).Milliseconds())
+				}
+				lastReceived = time.Now()
+				p = p.SetTime(lastReceived)
 				influxWriteAPI.WritePoint(p)
 			}
 		}
